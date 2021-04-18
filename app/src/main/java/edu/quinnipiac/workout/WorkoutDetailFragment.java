8634df0989f33164e921d4aa3202d7fa07b7d6ad
maxpetruzziello <mlpetruzziello@qu.edu>
2021-04-18 01:41:23 -0400

    Chapter 10: Workout App for iPad

--- app/src/main/java/edu/quinnipiac/workout/WorkoutDetailFragment.java
+++ app/src/main/java/edu/quinnipiac/workout/WorkoutDetailFragment.java
@@ -50,11 +50,20 @@ public class WorkoutDetailFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+        if (savedInstanceState != null) {
+            workoutId = savedInstanceState.getLong("workoutId");
         }
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
     }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        savedInstanceState.putLong("workoutId", workoutId);
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
